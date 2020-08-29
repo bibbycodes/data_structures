267f2d79bf2abcaaddecf67631ed39b53da95b10
Robert Griffith <49240490+bibbycodes@users.noreply.github.com>
2020-08-29 17:01:48 +0100

    add search, edit insert

--- python/linked_list.py
+++ python/linked_list.py
@@ -1,12 +1,16 @@
 class LinkedList:
-	def __init__(self, head = None):
-		self.head = head
+	def __init__(self, head_value = None):
+		self.head = Node(head_value)
 
 	def insert(self, value):
 		if self.head:
 			return self.head.insert(self.head, value)
 		self.head = Node(value)
 
+	def search(self, value):
+		if not self.head:
+			return None
+		return self.head.search(value, self.head)
 
 class Node:
 	def __init__(self, value):
@@ -14,16 +18,24 @@ class Node:
 		self.value = value
 
 	def insert(self, node, value):
-		if self.next_node == None:
-			self.next_node = Node(value)
-			return self.next_node
-		return self.insert(self.next, value)
+		if node.next_node == None:
+			node.next_node = Node(value)
+			return node.next_node
+		return node.insert(node.next_node, value)
+
+	def search(self, value, node):
+		if node.value == value:
+			return node
+		if node.next_node == None:
+			return None
+		return node.search(value, node.next_node)
 
 	
 
-l = LinkedList()
+l = LinkedList(2)
 l.insert(3)
 l.insert(4)
+print(l.search(4).value)
 
 print(l.head.value)
 print(l.head.next_node.value)
