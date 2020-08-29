b0bad861b88b7a5fd06fe273bb78ff794f334ad5
Robert Griffith <49240490+bibbycodes@users.noreply.github.com>
2020-08-29 16:30:47 +0100

    add insert function, working

--- python/linked_list.py
+++ python/linked_list.py
@@ -5,15 +5,25 @@ class LinkedList:
 	def insert(self, value):
 		if self.head:
 			return self.head.insert(self.head, value)
+		self.head = Node(value)
 
 
 class Node:
-	def __init__(self, value, next_node = None):
-		self.next_node = next_node
+	def __init__(self, value):
+		self.next_node = None
 		self.value = value
 
 	def insert(self, node, value):
-		if self.next_node == Node:
+		if self.next_node == None:
 			self.next_node = Node(value)
 			return self.next_node
 		return self.insert(self.next, value)
+
+	
+
+l = LinkedList()
+l.insert(3)
+l.insert(4)
+
+print(l.head.value)
+print(l.head.next_node.value)
