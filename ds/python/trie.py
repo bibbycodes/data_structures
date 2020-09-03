07a1d21d3ddf82a13608e45332e5696c0f5d79da
Robert Griffith <49240490+bibbycodes@users.noreply.github.com>
2020-09-03 20:32:02 +0100

    add search

--- ds/python/trie.py
+++ ds/python/trie.py
@@ -9,52 +9,36 @@ class Trie:
   def insert_recursively(self, node, word):
     if node.value == False:
       return
-    current_char = word[0] if word != [] else False
+    current_char = False if word == [] else word[0]
     current_node = Node(current_char)
     if current_char in node.children:
       return self.insert_recursively(node.children[current_char], word[1:])
     else:
       node.children[current_char] = current_node
       return self.insert_recursively(node.children[current_char], word[1:])
-
-
+  
+  def search(self, word):
+    letters_array = [char for char in word]
+    node = self.root
+    for letter in letters_array:
+      if letter in node.children:
+        node = node.children[letter]
+      else:
+        return False
+    return word
 
 class Node:
   def __init__(self, value):
     self.value = value
     self.children = {}
 
-  # def insert(self, letter, letters_array):
-  #   if letter is None:
-  #     return
-  #   if letter not in self.children:
-  #     letter_node = Node(letter)
-  #     self.children[letter] = letter_node
-  #     if len(letters_array) > 0:
-  #       return letter_node.insert(letters_array[0], letters_array[1:])
-  #   else:
-  #     letters_array = letters_array[1:]
-  #     return self.children[letter].insert(letters_array[0], letters_array[:1])
-
-  # def insert(self, node, word_array):
-  #   if node.value == None or word_array == []:
-  #     node.word_end = True
-  #     return
-  #   if node.value not in node.children:
-  #     new_node = Node(word_array[0])
-  #     node.children[word_array[0]] = new_node
-  #     if len(word_array) > 0:
-  #       return node.insert(new_node, word_array[1:])
-  #   else:
-  #     node = Node(node.children[node.value])
-  #     new_node = Node(word_array[0])
-  #     return node.insert(new_node, word_array[1:])
-
 
 t = Trie()
 t.insert_word("an")
 t.insert_word("abc")
 t.insert_word("abacus")
+t.insert_word("aboration")
+t.insert_word("abracadabra")
 t.insert_word("absolutely")
 print(t.root)
 print(t.root.children)
@@ -64,3 +48,4 @@ print(t.root.children['a'].children['b'].children['a'].children)
 print(t.root.children['a'].children['b'].children['a'].children['c'].children)
 print(t.root.children['a'].children['b'].children['a'].children['c'].children['u'].children)
 print(t.root.children['a'].children['b'].children['a'].children['c'].children['u'].children['s'].children)
+print(t.search("abacus"))
