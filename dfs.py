2c4867fb919b8fae75b17b35c9e000b9f37bda82
Robert Griffith <49240490+bibbycodes@users.noreply.github.com>
2020-09-12 23:14:08 +0100

    binary search

--- dfs.py
+++ dfs.py
@@ -10,8 +10,6 @@ def dfs_iterative(root):
 	visited = {}
 	stack.push(root)
 
-	print(root)
-
 	while not stack.is_empty():
 		current_node = stack.pop()
 		if current_node not in visited:
@@ -19,7 +17,6 @@ def dfs_iterative(root):
 			process(current_node, print)
 
 		for node in current_node.children:
-			print(node)
 			if node not in visited:
 				stack.push(node)
 
@@ -30,13 +27,12 @@ if __name__ == '__main__':
 		1 : [4, 3],
 		2 : [1],
 		3 : [2, 4],
-		4 : [],
+		4 : [1, 3],
 		5 : []
 	}
 
 	g = Graph(graph_dict)
 	root = list(g.nodes.keys())[0]
-	print(g.nodes)
 
 	dfs_iterative(root)
 
