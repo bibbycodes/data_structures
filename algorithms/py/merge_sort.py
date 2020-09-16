350c7deea532c406e56d3d84629b15cf7b5191e6
Robert Griffith <49240490+bibbycodes@users.noreply.github.com>
2020-09-16 20:33:17 +0100

    refactor merge => while True = code smell

--- algorithms/py/merge_sort.py
+++ algorithms/py/merge_sort.py
@@ -1,30 +1,30 @@
-def merge(array_1, array_2):
+def merge(left_array, right_array):
     final = []
-    i = 0
-    j = 0
-    while True:
-        if i >= len(array_2):
-            for k in range(j, len(array_1)):
-                final.append(array_1[k])
-            break
-        
-        if j >= len(array_1):
-            for l in range(i, len(array_2)):
-                final.append(array_2[l])
-            break
-
-        if array_2[i] <= array_1[j]:
-            final.append(array_2[i])
+    i = j = 0
+    while i < len(right_array) and j < len(left_array):
+        if right_array[i] <= left_array[j]:
+            final.append(right_array[i])
             i += 1
         else:
-            final.append(array_1[j])
+            final.append(left_array[j])
             j += 1
-    print(final)
+
+    for k in range(j, len(left_array)):
+        final.append(left_array[k])
+        
+    for l in range(i, len(right_array)):
+        final.append(right_array[l])
     return final
 
 def merge_sort(array):
     if len(array) == 1:
         return array
-    left = split(array[0:int(len(array) / 2)])
-    right = split(array[int(len(array) / 2):])
+    left = merge_sort(array[0:len(array) // 2])
+    right = merge_sort(array[len(array) // 2:])
     return merge(left, right)
+
+if __name__ == '__main__':
+    from random import random
+    array = [int(random() * 1000) for _ in range(1000)]
+    print(merge_sort(array))
+
