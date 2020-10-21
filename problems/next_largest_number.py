8cccb1bccc38f502e5ce69262a9d527a4c31ebe2
Robert Griffith <49240490+bibbycodes@users.noreply.github.com>
2020-10-21 16:10:28 +0100

    use reversed

--- problems/next_largest_number.py
+++ problems/next_largest_number.py
@@ -5,19 +5,21 @@ import math
 
 def next_bigger(number):
     array_of_digits = convert_number_to_array_of_digits_reversed(number)
-    j = len(array_of_digits) - 1
-    if array_of_digits[j] == 0:
-        j -= 1
+    j = 0
         
     for i in range(len(array_of_digits)):
+        jth = array_of_digits[j]
+        ith = array_of_digits[i]
+        if array_of_digits[j] == 0:
+            j += 1
         if array_of_digits[i] < array_of_digits[j]:
             swap_items(i, j, array_of_digits)
+            array_of_digits = list(reversed(array_of_digits))
             return convert_array_of_digits_to_number(array_of_digits)
         
         if array_of_digits[i] == array_of_digits[j]:
             j = i
     return -1
-
 
 def convert_number_to_array_of_digits_reversed(number):
     array_of_digits = []
@@ -35,8 +37,9 @@ def convert_array_of_digits_to_number(array_of_digits):
 def swap_items(index_1, index_2, array):
     array[index_1], array[index_2] = array[index_2], array[index_1]
 
+print(next_bigger((937206121)))
 failed = [(9876543210, -1), (1234567890, 1234567908), (937206121, 937206211), (3392506469761, 3392506471669), (10, -1), (121126001641, 121126004116), (315163, 315316)]
 for tuple_item in failed:
     print("actual output", next_bigger(tuple_item[0]), "correct output", tuple_item[1], "input", tuple_item[0])
-# print(convert_number_to_array_of_digits(123))
+# # print(convert_number_to_array_of_digits(123))
 
